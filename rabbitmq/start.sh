23b7f6ce0c2fba39676d0b3cd68d216f0876866a
tyjwin <30862823@qq.com>
2021-02-08 11:45:10 +0800

    修改配置

--- rabbitmq/start.sh
+++ rabbitmq/start.sh
@@ -13,5 +13,5 @@ docker run --name $(basename $(pwd)) -d --restart always \
         -e RABBITMQ_DEFAULT_VHOST='/' \
         -e RABBITMQ_DEFAULT_USER='admin' \
         -e RABBITMQ_DEFAULT_PASS='qwertyuiop' \
-        -e RABBITMQ_LOGS='/var/log/rabbitmq/rabbitmq_logs' 
+        -e RABBITMQ_LOGS='/var/log/rabbitmq/rabbitmq_logs' \
         -e RABBITMQ_SASL_LOGS='/var/log/rabbitmq/rabbitmq_sasl_logs' docker.io/rabbitmq:3.8-management

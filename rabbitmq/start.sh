040eca4286387b14955895ef50f915df4e3ea336
tyjwin <30862823@qq.com>
2021-02-08 11:44:10 +0800

    修改rabbitmq

--- rabbitmq/start.sh
+++ rabbitmq/start.sh
@@ -5,4 +5,13 @@ if [ $(docker network ls | grep dev | wc -l | xargs) = "0" ];then
         echo "Create docker network 'dev'."
         docker network create dev || echo "The docker network 'dev' is already created."
 fi
-docker run --name $(basename $(pwd)) -d --restart always --network dev -p 4369:4369 -p 5671-5672:5671-5672 -p 25672:25672 -v $(pwd)/etc:/etc/rabbitmq -v $(pwd)/data:/var/lib/rabbitmq -v $(pwd)/log:/var/log/rabbitmq -e RABBITMQ_DEFAULT_VHOST='/' -e RABBITMQ_DEFAULT_USER='admin' -e RABBITMQ_DEFAULT_PASS='qwertyuiop' -e RABBITMQ_LOGS='/var/log/rabbitmq/rabbitmq_logs' -e RABBITMQ_SASL_LOGS='/var/log/rabbitmq/rabbitmq_sasl_logs' rabbitmq
+docker run --name $(basename $(pwd)) -d --restart always \
+        --network dev -p 4369:4369 -p 5671-5672:5671-5672 -p 25672:25672 \
+        -v $(pwd)/etc:/etc/rabbitmq \
+        -v $(pwd)/data:/var/lib/rabbitmq \
+        -v $(pwd)/log:/var/log/rabbitmq \
+        -e RABBITMQ_DEFAULT_VHOST='/' \
+        -e RABBITMQ_DEFAULT_USER='admin' \
+        -e RABBITMQ_DEFAULT_PASS='qwertyuiop' \
+        -e RABBITMQ_LOGS='/var/log/rabbitmq/rabbitmq_logs' 
+        -e RABBITMQ_SASL_LOGS='/var/log/rabbitmq/rabbitmq_sasl_logs' docker.io/rabbitmq:3.8-management
